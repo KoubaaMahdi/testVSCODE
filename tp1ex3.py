2c808c1f6c5068077841c835a7ce5bd2e9f646c9
MahdiKoubaa <Mahdi.koubaa@outlook.com>
2022-07-08 10:05:36 +0100

    initialisation

---
+++ tp1ex3.py
@@ -0,0 +1,4 @@
+x=int(input("donner un terme\n"))
+for i in range (1,13):
+    print(x,"\n")
+    x*=3
---
+++ tp1ex4.py
@@ -0,0 +1,6 @@
+ch=input("donner une chaine\n")
+if 'a' in ch :
+    print("a existe dans ",ch)
+else :
+    print("a n'existe pas dans",ch)
+    
